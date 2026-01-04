8f8b673ad07501770da358fbb66394c58c9e683a
sachinstha9 <sachinshrestha7055@gmail.com>
2026-01-04 21:04:51 +1300

    test

--- main.py
+++ main.py
@@ -9,20 +9,19 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
-
 @app.get("/stock/{symbol}")
 def get_data(symbol: str):
     data = []
     
-    file_path = f"{symbol}.csv"
-    download_individual_stock_data(file_path)
+    # file_path = f"{symbol}.csv"
+    # download_individual_stock_data(file_path)
 
-    with open(file_path, newline="", encoding="utf-8") as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
+    # with open(file_path, newline="", encoding="utf-8") as csvfile:
+    #     reader = csv.DictReader(csvfile)
+    #     for row in reader:
+    #         data.append(row)
             
-    if os.path.exists(file_path):
-        os.remove(file_path)
+    # if os.path.exists(file_path):
+    #     os.remove(file_path)
 
-    return data
+    return {symbol: symbol}
